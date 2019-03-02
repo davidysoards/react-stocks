570f1e3430cac0358bccec542fab0f4cd676150d
David Soards <davids@gocodigo.com>
2019-03-01 23:33:55 -0500

    Updates

---
+++ precache-manifest.18d43173a8d2daa0e45af7ef4b3d89f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b39258e9dca35dab5ee49d1f3b1476d7",
+    "url": "/react-stocks/static/media/react-stocks-land.b39258e9.svg"
+  },
+  {
+    "revision": "da0cad5fc4112b370ecc",
+    "url": "/react-stocks/static/js/runtime~main.da0cad5f.js"
+  },
+  {
+    "revision": "6e0f37439b8b9198825d",
+    "url": "/react-stocks/static/js/main.6e0f3743.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/react-stocks/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "6e0f37439b8b9198825d",
+    "url": "/react-stocks/static/css/main.348ac8a6.chunk.css"
+  },
+  {
+    "revision": "78c4d4eb753eeff4b828250ecf4bd639",
+    "url": "/react-stocks/index.html"
+  }
+];
